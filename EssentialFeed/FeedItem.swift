0dc163664b65835d75a049e2e4f5bacb822e6fb9
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2021-04-25 11:04:26 +0530

    - Create feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Hitesh on 25/04/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id:UUID
+    let description:String?
+    let location:String?
+    let imageURL:URL
+}
