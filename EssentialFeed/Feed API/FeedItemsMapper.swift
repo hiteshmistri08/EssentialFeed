fe80cc9ae8404bf45b9fb49de50a7c5d0d52229e
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2021-10-17 11:31:50 +0530

    'FeedItemsMapper' has private intializer, Now single point access to the methods

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -28,6 +28,8 @@ internal final class FeedItemsMapper {
     
     private static var OK_200:Int { return 200 }
     
+    private init() { }
+    
     internal static func map(_ data:Data, from response:HTTPURLResponse) -> RemoteFeedLoader.Result {
         guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
             return .failure(RemoteFeedLoader.Error.invalidData)
