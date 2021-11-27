bf5d532ebbdaa1e96568b6c462e1d9f1454bd87f
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2021-11-27 13:23:32 +0530

    Add 'RemoteFeedItem' data transfer  representation to decouple the items mapper from 'FeedItem' data models

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -41,6 +41,6 @@ public final class LocalFeedLoader {
 
 private extension Array where Element == FeedItem {
     func toLocal() -> [LocalFeedItem] {
-        return self.map{ LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL) }
+        return map{ LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL) }
     }
 }
