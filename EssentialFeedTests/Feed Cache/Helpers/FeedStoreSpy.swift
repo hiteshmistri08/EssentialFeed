185a3b5b963617cdc044cd6ec9ef3a8d59a9d5b4
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2021-11-30 22:55:30 +0530

    Load command fils on retrieval error

--- EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -19,6 +19,7 @@ class FeedStoreSpy: FeedStore {
     
     private var deletionCompletions: [DeletionCompletion] = []
     private var insertionCompletions: [InsertionCompletion] = []
+    private var retrievalCompletions: [RetrievalCompletion] = []
 
     func deleteCachedFeed(completion:@escaping DeletionCompletion) {
         receivedMessages.append(.deleteCachedFeed)
@@ -46,7 +47,12 @@ class FeedStoreSpy: FeedStore {
         insertionCompletions[index](nil)
     }
     
-    func retrieve() {
+    func retrieve(completion:@escaping RetrievalCompletion) {
+        retrievalCompletions.append(completion)
         receivedMessages.append(.retrieve)
+    }
+    
+    func completeRetrival(with error: Error, at index: Int = 0) {
+        retrievalCompletions[index](error)
     }
 }
