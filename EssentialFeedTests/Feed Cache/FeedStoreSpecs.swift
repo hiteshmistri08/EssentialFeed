d5d4dced18d516459edf01cdb53b10d27569de3c
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2021-12-23 11:02:26 +0530

    Extract 'FeedStoreSpecs' checks into reusable assertions

--- EssentialFeedTests/Feed Cache/FeedStoreSpecs.swift
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs.swift
@@ -11,11 +11,11 @@ protocol FeedStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache()
     func test_retrieve_hasNoSideEffectsOnEmptyCache()
     func test_retrieve_deliversFoundValuesOnNonEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCahce()
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
 
     func test_insert_deliversNoErrorOnEmptyCache()
     func test_insert_deliversNoErrorOnNonEmptyCache()
-    func test_insert_ovveridesPreviouslyInsertedCacheValues()
+    func test_insert_overridesPreviouslyInsertedCacheValues()
 
     func test_delete_deliversNoErrorOnEmptyCache()
     func test_delete_hasNoSideEffectsOnEmptyCache()
